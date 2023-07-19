715180c0be6b7f627ef684bba6e6ada8793181e4
aarjavjain <jainaarjav24@gmail.com>
2023-07-19 23:17:55 +0530

    testing

---
+++ genericRubikscube.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Aarjav Jain on 19/07/23.
+//
+
+#include "genericRubikscube.h"
---
+++ genericRubikscube.h
@@ -0,0 +1,14 @@
+//
+// Created by Aarjav Jain on 19/07/23.
+//
+
+#ifndef RUBIKSCUBESOLVER_GENERICRUBIKSCUBE_H
+#define RUBIKSCUBESOLVER_GENERICRUBIKSCUBE_H
+
+
+class genericRubikscube {
+
+};
+
+
+#endif //RUBIKSCUBESOLVER_GENERICRUBIKSCUBE_H
